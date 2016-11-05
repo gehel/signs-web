589607663db036b3aae01b3b5029bf7864720573
Guillaume Lederrey <glederrey@wikimedia.org>
2016-11-05 22:22:10 +0100

    remove tests which rely on specific elasticsearch state

--- src/test/java/ch/ledcom/signs/SignRepositoryTest.java
+++ src/test/java/ch/ledcom/signs/SignRepositoryTest.java
@@ -4,8 +4,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,18 +23,6 @@ public class SignRepositoryTest {
         assertThat(hello.getName()).isEqualTo("hello");
         assertThat(hello.getDescription()).isEqualTo("a greeting");
         assertThat(hello.getImage()).isEqualTo("some-image");
-    }
-
-    @Test
-    public void findByDescription() {
-        Page<Sign> fruits = signRepository.findByDescription("fruit", new PageRequest(0, 3));
-        assertThat(fruits).hasSize(1);
-    }
-
-    @Test
-    public void findByAllFields() {
-        Page<Sign> fruits = signRepository.findByAllFields("fruit", new PageRequest(0, 3));
-        assertThat(fruits).hasSize(1);
     }
 
 }
